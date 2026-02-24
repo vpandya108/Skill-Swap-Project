995375e51af9e6ba3971bc667dfb98530d40564d
vpandya108 <vpandya108@rku.ac.in>
2026-02-24 13:23:17 +0530

    Add AboutUsController with Index action for About Us page
    Added a new AboutUsController in the Skill_Swap_Project.Controllers namespace. The controller includes an Index action that returns a view, providing backend support for the application's "About Us" page.

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Swap_Project.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
