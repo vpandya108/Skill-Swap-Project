4e55f07b3ec9548865f54fe4b4e9fda8ea9a4b02
vpandya108 <vpandya108@rku.ac.in>
2026-02-25 10:15:01 +0530

    Add DashBoardController with DashBoardview action
    Added DashBoardController in Skill_Swap_Project.Controllers namespace. The controller includes a DashBoardview action method that returns a view. Included Microsoft.AspNetCore.Mvc using directive.

---
+++ Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Swap_Project.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult DashBoardview()
+        {
+            return View();
+        }
+    }
+}
