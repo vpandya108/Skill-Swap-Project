6876e9a4c518ddb6983177446f2c96b16b5d80fa
vpandya108 <vpandya108@rku.ac.in>
2026-02-25 08:22:57 +0530

    Add Account/Login controllers & modernize homepage styling
    Introduced AccountController and LoginController with basic actions. Restyled Index.cshtml using Tailwind-like classes, replacing Bootstrap. Updated layout, colors, and typography for hero, story, values, and CTA sections. Enhanced button visuals and prepared for new account/login features.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Swap_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult CreateProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Swap_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
