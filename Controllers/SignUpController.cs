5d22fc34b83d269466cc34c628af1c6c708a6786
vpandya108 <vpandya108@rku.ac.in>
2026-02-25 11:06:43 +0530

    Add SignUpController with GET/POST actions for sign-up
    Introduced a new SignUpController with actions to render the sign-up view (GET) and handle user registration (POST). The POST action currently includes a placeholder for saving user data and redirects to the profile creation page after successful sign-up.

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Swap_Project.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(string fullname, string email, string password)
+        {
+            // Here you can save user to database
+
+            // Then redirect to Profile Creation page
+            //
+            return RedirectToAction("Account", "CreateProfile");
+        }
+    }
+}
