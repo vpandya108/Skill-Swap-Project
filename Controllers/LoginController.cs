467ba985f908250aaee376fd39337efe5dcc0e4f
vpandya108 <vpandya108@rku.ac.in>
2026-03-09 20:01:30 +0530

    add validation

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,8 +4,25 @@ namespace Skill_Swap_Project.Controllers
 {
     public class LoginController : Controller
     {
+        // Shows Login Page
+        [HttpGet]
         public IActionResult Login()
         {
+            return View();
+        }
+
+        // Runs when Login button is clicked
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            // Temporary login check (you can connect DB later)
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                // Redirect to Dashboard
+                return RedirectToAction("DashBoardview", "Dashboard");
+            }
+
+            ViewBag.Error = "Invalid Login";
             return View();
         }
     }
