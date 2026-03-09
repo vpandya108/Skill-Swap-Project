1282973c2a20833df46988220cc40139b5981732
vpandya108 <vpandya108@rku.ac.in>
2026-03-09 18:32:54 +0530

    add message page for user communicate

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Swap_Project.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
