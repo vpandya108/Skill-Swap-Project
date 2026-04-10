bd0c1725091e95249a3c35a8a249708e4ae1e963
Nisha Solanki <124144498+nishasolanki24@users.noreply.github.com>
2026-04-10 11:06:58 +0530

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,7 +8,6 @@ namespace Skill_Swap_Project.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
         public DbSet<User> Users { get; set; }
         public DbSet<SwapRequest> SwapRequests { get; set; }
     }
