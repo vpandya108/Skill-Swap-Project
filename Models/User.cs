602e59350c5df8828e57e58edcc7f16d596d5bb5
Nisha Solanki <124144498+nishasolanki24@users.noreply.github.com>
2026-04-10 11:03:29 +0530

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -9,11 +9,9 @@ namespace Skill_Swap_Project.Models
 
         [Required]
         public string FullName { get; set; } = string.Empty;
-
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
-
         [Required]
         public string Password { get; set; } = string.Empty;
 
